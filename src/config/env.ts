82854932ae30d7ecda385f1542ab252ea40f089c
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2025-12-28 18:55:46 +0700

    feat(env): add `DATABASE_URL` to environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,5 @@ import 'dotenv/config'
 export const env = {
   PORT: Number(process.env.PORT) || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL,
 }
